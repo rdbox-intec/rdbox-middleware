0be9dbbfe546783afb3c9f0a261264163a9a45eb
intec <446-rdbox@intec.co.jp>
2018-05-15 11:49:53 +0900

    Release 0.0.9
     - modify wlan10 path.

--- boot/rdbox-first_session.bash
+++ boot/rdbox-first_session.bash
@@ -21,7 +21,7 @@ elif [[ $hname =~ $regex_slave ]]; then
   /bin/systemctl enable rdbox-boot.service
   /bin/systemctl restart rdbox-boot.service
 else
-  cp -rf /etc/rdbox/networks/interface/wlan10 /etc/network/interfaces.d/wlan10
+  cp -rf /etc/rdbox/templates/interface/wlan10 /etc/network/interfaces.d/wlan10
   ln -s /etc/rdbox/wpa_supplicant_be.conf /etc/wpa_supplicant/wpa_supplicant.conf
   /etc/init.d/networking restart
 fi
