ad99ac12a788aa04c4e01157fc57726e57e30efc
intec <446-rdbox@intec.co.jp>
2018-06-01 15:39:30 +0900

    Release 0.0.10 - fix for not RDBOX Modules

--- boot/rdbox-first_session.bash
+++ boot/rdbox-first_session.bash
@@ -4,9 +4,8 @@ regex_master='^.*master.*'
 regex_slave='^.*slave.*'
 hname=`/bin/hostname`
 
-/usr/bin/lsusb -t | /bin/grep -B 1 rt2800usb | /bin/grep -o "Port [0-9]*" | /bin/grep -o "[0-9]*" | /usr/bin/python /opt/rdbox/boot/rdbox-bind_unbind_dongles.py
-
 if [[ $hname =~ $regex_master ]]; then
+  /usr/bin/lsusb -t | /bin/grep -B 1 rt2800usb | /bin/grep -o "Port [0-9]*" | /bin/grep -o "[0-9]*" | /usr/bin/python /opt/rdbox/boot/rdbox-bind_unbind_dongles.py
   mv /etc/network/interfaces /etc/network/interfaces.org
   cp -rf /etc/rdbox/templates/interface/master /etc/network/interfaces
   /etc/init.d/networking restart
@@ -15,6 +14,7 @@ if [[ $hname =~ $regex_master ]]; then
   /bin/systemctl enable dnsmasq.service
   /bin/systemctl restart dnsmasq.service
 elif [[ $hname =~ $regex_slave ]]; then
+  /usr/bin/lsusb -t | /bin/grep -B 1 rt2800usb | /bin/grep -o "Port [0-9]*" | /bin/grep -o "[0-9]*" | /usr/bin/python /opt/rdbox/boot/rdbox-bind_unbind_dongles.py
   mv /etc/network/interfaces /etc/network/interfaces.org
   cp -rf /etc/rdbox/templates/interface/slave /etc/network/interfaces
   /etc/init.d/networking restart
@@ -22,7 +22,7 @@ elif [[ $hname =~ $regex_slave ]]; then
   /bin/systemctl restart rdbox-boot.service
 else
   cp -rf /etc/rdbox/templates/interface/wlan10 /etc/network/interfaces.d/wlan10
-  ln -s /etc/rdbox/wpa_supplicant_be.conf /etc/wpa_supplicant/wpa_supplicant.conf
+  ln -s /etc/rdbox/wpa_supplicant_ap_bg.conf /etc/wpa_supplicant/wpa_supplicant.conf
   /etc/init.d/networking restart
 fi
 
