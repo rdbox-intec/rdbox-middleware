b396febb1400afcd1f973cbebe611a1c3c49d179
intec <446-rdbox@intec.co.jp>
2018-11-06 13:55:49 +0900

    Release 0.0.20
    * Separate NIC configuration per interface.
    * Separate NIC settings and firewall settings.
    * Mesh Set to prevent the dongle of its own node from connecting to the Wi-Fi network.

--- boot/rdbox-boot.bash
+++ boot/rdbox-boot.bash
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-/bin/bash /opt/rdbox/boot/rdbox-boot_sub.bash > /var/log/rdbox_boot.log 2>&1
+regex_master='^.*master.*'
+regex_slave='^.*slave.*'
+regex_vpnbridge='^.*vpnbridge.*'
+hname=`/bin/hostname`
+
+if [[ $hname =~ $regex_master ]]; then
+  source /etc/rdbox/network/iptables
+  /bin/bash /opt/rdbox/boot/rdbox-boot_sub.bash > /var/log/rdbox_boot.log 2>&1
+elif [[ $hname =~ $regex_slave ]]; then
+  /bin/bash /opt/rdbox/boot/rdbox-boot_sub.bash > /var/log/rdbox_boot.log 2>&1
+else
+  echo "OK!!"
+fi
+
+exit 0
+~      
+
--- boot/rdbox-first_session.bash
+++ boot/rdbox-first_session.bash
@@ -11,7 +11,8 @@ hname=`/bin/hostname`
 if [[ $hname =~ $regex_master ]]; then
   /usr/bin/lsusb -t | /bin/grep -B 1 rt2800usb | /bin/grep -o "Port [0-9]*" | /bin/grep -o "[0-9]*" | /usr/bin/python /opt/rdbox/boot/rdbox-bind_unbind_dongles.py
   mv /etc/network/interfaces /etc/network/interfaces.org
-  cp -rf /etc/rdbox/templates/interface/master /etc/network/interfaces
+  ln -s /etc/rdbox/network/interfaces /etc/network/interfaces
+  cp -n /etc/rdbox/network/interfaces.d/master/* /etc/rdbox/network/interfaces.d/current
   /etc/init.d/networking restart
   /bin/systemctl enable rdbox-boot.service
   /bin/systemctl restart rdbox-boot.service
@@ -23,13 +24,23 @@ if [[ $hname =~ $regex_master ]]; then
   /bin/systemctl start nfs-kernel-server.service
   sed -i '/^#ListenAddress 0.0.0.0$/c ListenAddress 192.168.179.1' /etc/ssh/sshd_config
   /etc/init.d/ssh restart
+  http_proxy_size=`wc -c /etc/transproxy/http_proxy | awk '{print $1}'`
+  no_proxy_size=`wc -c /etc/transproxy/no_proxy | awk '{print $1}'`
+  if [ $http_proxy_size -gt 5 ] && [ $no_proxy_size -gt 5 ]; then
+    /bin/systemctl enable transproxy.service
+    /bin/systemctl restart transproxy.service
+  else
+    /bin/systemctl disable transproxy.service
+    /bin/systemctl stop transproxy.service
+  fi
 elif [[ $hname =~ $regex_slave ]]; then
   /usr/bin/lsusb -t | /bin/grep -B 1 rt2800usb | /bin/grep -o "Port [0-9]*" | /bin/grep -o "[0-9]*" | /usr/bin/python /opt/rdbox/boot/rdbox-bind_unbind_dongles.py
-  echo "127.0.1.1 $hname" >> /etc/hosts
-  echo "127.0.1.1 $hname" >> /etc/cloud/templates/hosts.debian.tmpl
   mv /etc/network/interfaces /etc/network/interfaces.org
-  cp -rf /etc/rdbox/templates/interface/slave /etc/network/interfaces
+  ln -s /etc/rdbox/network/interfaces /etc/network/interfaces
+  cp -n /etc/rdbox/network/interfaces.d/slave/* /etc/rdbox/network/interfaces.d/current
+  /sbin/ifconfig wlan0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' > /etc/rdbox/hostapd_be.deny
   /etc/init.d/networking restart
+  /sbin/dhclient br0
   /bin/systemctl enable rdbox-boot.service
   /bin/systemctl restart rdbox-boot.service
   /bin/systemctl disable systemd-networkd-wait-online.service
@@ -37,10 +48,12 @@ elif [[ $hname =~ $regex_slave ]]; then
   sed -i '/^#timeout 60;$/c timeout 10;' /etc/dhcp/dhclient.conf
 elif [[ $hname =~ $regex_vpnbridge ]]; then
   mv /etc/network/interfaces /etc/network/interfaces.org
-  cp -rf /etc/rdbox/templates/interface/vpnbridge /etc/network/interfaces
+  ln -s /etc/rdbox/network/interfaces /etc/network/interfaces
+  cp -n /etc/rdbox/network/interfaces.d/vpnbridge/* /etc/rdbox/network/interfaces.d/current
   ln -s /etc/rdbox/wpa_supplicant_ap_bg.conf /etc/wpa_supplicant/wpa_supplicant.conf
   /etc/init.d/networking restart
   /sbin/ifup wlan10
+  /sbin/dhclient wlan10 
   /sbin/ip addr del `ip -f inet -o addr show eth0|cut -d\  -f 7 | cut -d/ -f 1`/24 dev eth0
   /bin/systemctl enable softether-vpnbridge.service
   /bin/systemctl restart softether-vpnbridge.service
@@ -48,10 +61,13 @@ elif [[ $hname =~ $regex_vpnbridge ]]; then
   /usr/bin/vpncmd localhost:443 -server -in:/usr/local/etc/vpnbridge.in
   /bin/systemctl restart softether-vpnbridge.service
 else
-  cp -rf /etc/rdbox/templates/interface/wlan10 /etc/network/interfaces.d/wlan10
+  mv /etc/network/interfaces /etc/network/interfaces.org
+  ln -s /etc/rdbox/network/interfaces /etc/network/interfaces
+  cp -n /etc/rdbox/network/interfaces.d/others/* /etc/rdbox/network/interfaces.d/current
   ln -s /etc/rdbox/wpa_supplicant_ap_bg.conf /etc/wpa_supplicant/wpa_supplicant.conf
   /etc/init.d/networking restart
   /sbin/ifup wlan10
+  /sbin/dhclient wlan10 
 fi
 
 exit 0
