9a95c29a277ca4c50c819a75eca2bd4b6d7c1aa1
fukuta-tatsuya-intec <fukuta_tatsuya@intec.co.jp>
2020-06-26 08:11:14 +0900

    Added support for specifying the branch name in the build script.

--- build.sh
+++ build.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-if [ $# != 2 ]; then
+if [ $# != 3 ]; then
   echo "Invalid Argment."
   echo " You need to specify the version number. (like 0.0.1)"
   echo " You need to specify Architect. (like armhf, amd64)"
+  echo " You neet to specify git branch. (like master, feature/app, develop...)"
   exit 1
 fi
 
 version_no=$1
 architect_code=$2
-git pull origin master
-git checkout master
+branch=$3
+git pull origin "${branch}"
+git checkout "${branch}"
 commit_id=$(git rev-parse HEAD)
 git archive --format=tar.gz --prefix=rdbox/ -o ../rdbox_"${version_no}".orig.tar.gz "${commit_id}"
 
@@ -28,9 +30,9 @@ rm -rf ../build-area/
 gbp buildpackage --git-pristine-tar-commit --git-export-dir=../build-area -S -sd
 
 # need sudo
-if [ $architect_code = "armhf" ]; then
+if [ "$architect_code" = "armhf" ]; then
   sudo OS=raspbian DIST=buster ARCH=armhf pbuilder --build ../build-area/rdbox_"${version_no}".dsc
-elif [ $architect_code = "amd64" ]; then
+elif [ "$architect_code" = "amd64" ]; then
   sudo OS=debian DIST=buster ARCH=amd64 pbuilder --build ../build-area/rdbox_"${version_no}".dsc
 else
   sudo OS=raspbian DIST=buster ARCH=armhf pbuilder --build ../build-area/rdbox_"${version_no}".dsc
